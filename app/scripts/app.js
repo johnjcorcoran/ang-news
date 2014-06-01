06e05375510553e29834ca3e210b1808ce9e9a0e
JohnJCorcoran <jcyorkshire@gmail.com>
2014-06-01 18:24:16 +0100

    hello world

---
+++ app/scripts/app.js
@@ -0,0 +1,19 @@
+/* global app:true */
+'use strict';
+
+var app = angular.module('angNewsApp', [
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngRoute'
+  ]);
+  app.config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/posts.html',
+        controller: 'PostsCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
